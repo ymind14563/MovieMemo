c9f99bb86114d7be675ab37dcef0498a61051991
pouryourlove <lqu134@naver.com>
2024-07-10 22:18:17 +0900

    feat:modal

--- public/js/main.js
+++ public/js/main.js
@@ -20,10 +20,35 @@ const firstSwiper = new Swiper(".first-swiper", {
 });
 
 const secondSwiper = new Swiper(".second-swiper", {
-  slidesPerView: 4,
+  slidesPerView: 6,
+  spaceBetween: 0,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// modal
+
+const loginBtn = document.querySelector("#openLoginModal");
+const modal = document.querySelector(".loginModal");
+const closeBtn = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+
+loginBtn.addEventListener("click", function () {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+  }
 });
