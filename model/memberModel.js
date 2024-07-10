ee7c528910970ab03adda51310e2006c5d14b2b7
munjuin <raiawa1212@gmail.com>
2024-07-10 13:58:41 +0900

    feat: develop_memberModel

--- model/memberModel.js
+++ model/memberModel.js
@@ -1,7 +1,56 @@
-// - 로그인
-//     - getMember()
-// - 회원
-//     - postMember() - 회원가입
-//     - getMember() - 로그인, 회원 검색 or 회원검색용 함수를 따로 만들거나 ex. getFindMember()
-//     - patchMember() - 비밀번호 수정
-//     - deleteMember() - 회원 탈퇴, 회원 강퇴
+module.exports = (sequelize, DataTypes) => {
+  const Member = sequelize.define(
+    "Member",
+    {
+      member_id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      nick: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING(70),
+        allowNull: false,
+      },
+      is_active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      is_admin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+    {
+      tableName: "MEMBER",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
+
+  // 관계 설정
+  Member.associate = (models) => {
+    Member.hasMany(models.Review, { foreignKey: "member_id" });
+  };
+
+  return Member;
+};
