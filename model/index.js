73545af62b449b5516c21952da2a269aba224a7b
munjuin <raiawa1212@gmail.com>
2024-07-10 16:08:25 +0900

    fix: add memberModel in index.js

--- model/index.js
+++ model/index.js
@@ -1,19 +1,25 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 // 모델 불러오기
-const Movie = require('./movieModel')(sequelize, Sequelize);
-const Genre = require('./genreModel')(sequelize, Sequelize);
-const MovieGenre = require('./movieGenreModel')(sequelize, Sequelize);
+const Movie = require("./movieModel")(sequelize, Sequelize);
+const Genre = require("./genreModel")(sequelize, Sequelize);
+const MovieGenre = require("./movieGenreModel")(sequelize, Sequelize);
+const Member = require("./memberModel")(sequelize, Sequelize);
 
 // db 객체에 모델 추가
 db.sequelize = sequelize;
@@ -22,13 +28,14 @@ db.Sequelize = Sequelize;
 db.Movie = Movie;
 db.Genre = Genre;
 db.MovieGenre = MovieGenre;
-
+db.Member = member;
 
 // 모델 동기화
 const syncModels = async () => {
   await Movie.sync();
   await Genre.sync();
   await MovieGenre.sync();
+  await Member.sync();
 };
 
 db.syncModels = syncModels;
