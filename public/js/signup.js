6522aedbaa65b167c577e2523d2d18cd67db742c
pouryourlove <lqu134@naver.com>
2024-07-15 15:37:25 +0900

    feat:회원가입 에러메세지

--- public/js/signup.js
+++ public/js/signup.js
@@ -15,7 +15,46 @@ form.addEventListener('submit',async function(e) {
   try {
     const response = await axios.post("/register", data);
     console.log(response);
+    window.location.href = '/' //main page when successed
   } catch (error) {
-    console.log(error);
+    if(error.response && error.response.status === 400){
+      const errors = error.response.data.errors; //array
+      displayErrors(errors)
+    } else {
+      console.log(error);
+    }
+    
   }
 })
+
+function displayErrors(errors){
+  const nameError = document.getElementById('name-error')
+  const passwordError = document.getElementById('password-error')
+  const nickError = document.getElementById('nick-error')
+  // const confirmPasswordError = document.getElementById('confirmPassword-error')
+
+  //clear the previous error messages
+  nameError.textContent = '';
+  nickError.textContent = '';
+  passwordError.textContent = '';
+  // confirmPasswordError.textContent = '';
+
+  errors.forEach((error) => {
+    switch(error.param){
+      case 'name':
+        nameError.textContent = error.msg;
+        break;
+      case 'nick':
+        nickError.textContent = error.msg;
+        break;
+      case 'password':
+        passwordError.textContent = error.msg;
+        break;
+      // case 'confirmPassword':
+      //   confirmPasswordError.textContent = error.msg;
+      //   break;
+        default:
+          break;
+    }
+  })
+}
