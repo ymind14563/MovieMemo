1ff94c5111deba641e5fb479626252620b888e7b
anemoa <anemoia.yves@gmail.com>
2024-07-17 13:18:08 +0900

    feat: 영화 API 호출

--- public/js/review.js
+++ public/js/review.js
@@ -4,6 +4,8 @@
 //리뷰 등록
 // 리뷰 등록 버튼 postReview()
 
+
+// 리뷰 모달 버튼
 const modal = document.querySelector('.movie_review_modal_bg');
 
 const postReview = () => {
@@ -15,3 +17,24 @@ const closeModal = () => {
    modal.classList.add('hidden');
    modal.classList.remove('visible');
 };
+
+
+
+// 영화 api 불러오기 관련
+const key = "BNUTWI8LOC2C99593QD4";
+const movieUrl = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&title="파묘"&ServiceKey=${key}`;
+
+async function movieInfo(){
+
+  try{
+      const movie = await axios.get(movieUrl);
+    
+      console.log("영화 데이터 전체", movie);
+
+      
+  }catch(error){
+    console.log(error);
+  }
+}
+
+movieInfo();
