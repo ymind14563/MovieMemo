51b987a8ab48868f33950217be86344ea6e5557b
ymind14563 <ymind14563@gmail.com>
2024-07-19 23:59:42 +0900

    refactor: sort

--- utils/sort.js
+++ utils/sort.js
@@ -2,12 +2,22 @@ const sort = (sortBy) => {
     let order = [];
 
     // 정렬 기준
-    if (sortBy === 'latest') { 
-        order = [['createdAt', 'DESC']]; // 최신순
-    } else if (sortBy === 'oldest') { 
-        order = [['createdAt', 'ASC']]; // 등록일순
-    } else if (sortBy === 'rating') { 
-        order = [['reviewMovieRating', 'DESC'], ['createdAt', 'DESC']]; // 평점순, 평점이 같다면 최신순
+    switch (sortBy) {
+        case 'latest':
+            order = [['createdAt', 'DESC']]; // 최신순
+            break;
+        case 'oldest':
+            order = [['createdAt', 'ASC']]; // 등록일순
+            break;
+        case 'rating':
+            order = [['reviewMovieRating', 'DESC'], ['createdAt', 'DESC']]; // 평점순, 평점이 같다면 최신순
+            break;
+        case 'popular':
+            order = [['likeCount', 'DESC'], ['createdAt', 'DESC']]; // 좋아요 순, 좋아요가 같다면 최신순
+            break;
+        default:
+            order = [['createdAt', 'DESC']]; // 기본 정렬 기준, 최신순
+            break;
     }
 
     return order;
