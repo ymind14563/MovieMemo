4d54146785d9f3abd1c2c7672b550d9e35667bf8
ymind14563 <ymind14563@gmail.com>
2024-07-20 21:17:37 +0900

    feat: routes admin page

--- middleware/routerMiddleware.js
+++ middleware/routerMiddleware.js
@@ -2,10 +2,12 @@ const RReview = require('../routes/RReview');
 const RMovie = require('../routes/RMovie');
 const RMember = require('../routes/RMember');
 const RMyPage = require('../routes/RMyPage');
+const RAdminPage = require('../routes/RAdminPage');
 
 exports.routerMiddleware=(app)=>{
   app.use("/member", RMember);
   app.use("/review", RReview);
   app.use("/movie", RMovie);
   app.use("/mypage", RMyPage);
+  app.use("/adminpage", RAdminPage);
 }
