1eaec0dee1ccdcc483570cb678dbbccd602a4022
pouryourlove <lqu134@naver.com>
2024-07-20 12:06:02 +0900

    feat:getting data

--- public/js/main.js
+++ public/js/main.js
@@ -19,6 +19,22 @@ const firstSwiper = new Swiper(".first-swiper", {
   },
 });
 
+//first slide
+
+async function fetchTopReviews(limit = 3) {
+  try {
+    const response = await axios.get(`/review/getTopReviews/${limit}`);
+    console.log(response.data)
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching top reviews:", error);
+    return [];
+  }
+}
+
+fetchTopReviews().then((data) => console.log("Fetched data:", data));
+
+
 // modal
 
 const loginBtn = document.querySelector("#openLoginModal");
@@ -70,131 +86,131 @@ document.addEventListener("DOMContentLoaded", () => {
  // main posters api 
 
 
-function shuffleArraysInSync(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    throw new Error('Arrays must have the same length');
-  }
+// function shuffleArraysInSync(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     throw new Error('Arrays must have the same length');
+//   }
 
-  for (let i = arr1.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    // Swap elements in both arrays
-    [arr1[i], arr1[j]] = [arr1[j], arr1[i]];
-    [arr2[i], arr2[j]] = [arr2[j], arr2[i]];
-  }
-}
+//   for (let i = arr1.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     // Swap elements in both arrays
+//     [arr1[i], arr1[j]] = [arr1[j], arr1[i]];
+//     [arr2[i], arr2[j]] = [arr2[j], arr2[i]];
+//   }
+// }
 
-// Function to create sections with shuffled genres and section classes
-async function createSection() {
-  const genres = ['액션', '코메디', '멜로','드라마','판타지','SF','어드벤처'];
-  const sectionClasses = ['action-section', 'comedy-section', 'romance-section','drama-section','fantasy-section', 'sf-section','adventure-section'];
+// // Function to create sections with shuffled genres and section classes
+// async function createSection() {
+//   const genres = ['액션', '코메디', '멜로','드라마','판타지','SF','어드벤처'];
+//   const sectionClasses = ['action-section', 'comedy-section', 'romance-section','drama-section','fantasy-section', 'sf-section','adventure-section'];
 
-  shuffleArraysInSync(genres, sectionClasses);
+//   shuffleArraysInSync(genres, sectionClasses);
 
-  const sections = document.querySelectorAll('.second-swiper');
+//   const sections = document.querySelectorAll('.second-swiper');
 
 
-  sections.forEach(async(section, index) => {
-    const genre = genres[index];
-    const sectionClass = sectionClasses[index];
-    let moviesFromBackend;
-    section.innerHTML = ''; // Clear existing content
+//   sections.forEach(async(section, index) => {
+//     const genre = genres[index];
+//     const sectionClass = sectionClasses[index];
+//     let moviesFromBackend;
+//     section.innerHTML = ''; // Clear existing content
 
-    // Set the genre as the section title (h3 element)
-    const h3 = document.createElement('h3');
-    h3.textContent = genre;
-    section.appendChild(h3);
-    // Fetch movies by genre and populate swiper slides
-    const result = await axios({
-      method : 'get',
-      url : `/movie/genreList/${genre}`,
-    }).then((res)=>{
-      moviesFromBackend = res.data.data;
-    })
-    const swiperWrapper = document.createElement('div');
-    swiperWrapper.classList.add('swiper-wrapper');
-    await moviesFromBackend.forEach((movie) => {
-      const posterUrls = movie.posterUrl
-      dynamicSlides(posterUrls, sectionClass, movie, swiperWrapper);
-      });
+//     // Set the genre as the section title (h3 element)
+//     const h3 = document.createElement('h3');
+//     h3.textContent = genre;
+//     section.appendChild(h3);
+//     // Fetch movies by genre and populate swiper slides
+//     const result = await axios({
+//       method : 'get',
+//       url : `/movie/genreList/${genre}`,
+//     }).then((res)=>{
+//       moviesFromBackend = res.data.data;
+//     })
+//     const swiperWrapper = document.createElement('div');
+//     swiperWrapper.classList.add('swiper-wrapper');
+//     await moviesFromBackend.forEach((movie) => {
+//       const posterUrls = movie.posterUrl
+//       dynamicSlides(posterUrls, sectionClass, movie, swiperWrapper);
+//       });
     
-    section.appendChild(swiperWrapper);
+//     section.appendChild(swiperWrapper);
 
-    const prevButton = document.createElement('div');
-    prevButton.classList.add('swiper-button-prev');
-    section.appendChild(prevButton);
+//     const prevButton = document.createElement('div');
+//     prevButton.classList.add('swiper-button-prev');
+//     section.appendChild(prevButton);
 
-    const nextButton = document.createElement('div');
-    nextButton.classList.add('swiper-button-next');
-    section.appendChild(nextButton);
+//     const nextButton = document.createElement('div');
+//     nextButton.classList.add('swiper-button-next');
+//     section.appendChild(nextButton);
 
-    new Swiper(section, {
-      slidesPerView: 6,
-      spaceBetween: 0,
-      loop: true,
-      navigation: {
-        nextEl: section.querySelector('.swiper-button-next'),
-        prevEl: section.querySelector('.swiper-button-prev'),
-      },
-    });
+//     new Swiper(section, {
+//       slidesPerView: 6,
+//       spaceBetween: 0,
+//       loop: true,
+//       navigation: {
+//         nextEl: section.querySelector('.swiper-button-next'),
+//         prevEl: section.querySelector('.swiper-button-prev'),
+//       },
+//     });
 
 
-  }); 
+//   }); 
 
   
-}
+// }
 
-async function fetchMoviesByGenre(genre){
-  try {
-    const response = await axios.get(`/movie/genreList/${(genre)}`)
-    return response.data.movies;
-  }catch(error){
-    console.error(error)
-    throw error
-  }
-}
-async function sendMovieIdToBackend(movieId) {
-  try {
-    const response = await axios.get(
-      `/movie/movieInfo/${(movieId)}`
-    );
-    // console.log("Movie ID sent to backend successfully", response.data);
-  } catch (error) {
-    console.error("Error sending movie ID to backend", error);
-  }
-}
+// async function fetchMoviesByGenre(genre){
+//   try {
+//     const response = await axios.get(`/movie/genreList/${(genre)}`)
+//     return response.data.movies;
+//   }catch(error){
+//     console.error(error)
+//     throw error
+//   }
+// }
+// async function sendMovieIdToBackend(movieId) {
+//   try {
+//     const response = await axios.get(
+//       `/movie/movieInfo/${(movieId)}`
+//     );
+//     // console.log("Movie ID sent to backend successfully", response.data);
+//   } catch (error) {
+//     console.error("Error sending movie ID to backend", error);
+//   }
+// }
 
 
-function dynamicSlides(url, sectionClass, movieData, swiperWrapper) {
-  const swiperSlide = document.createElement('div');
-  swiperSlide.classList.add('swiper-slide');
+// function dynamicSlides(url, sectionClass, movieData, swiperWrapper) {
+//   const swiperSlide = document.createElement('div');
+//   swiperSlide.classList.add('swiper-slide');
 
-  const cardContainer = document.createElement('div');
-  cardContainer.classList.add('card-container');
+//   const cardContainer = document.createElement('div');
+//   cardContainer.classList.add('card-container');
 
-  const cardContent = document.createElement('div');
-  cardContent.classList.add('card-content');
+//   const cardContent = document.createElement('div');
+//   cardContent.classList.add('card-content');
 
-  const image = document.createElement('img');
-  image.src = url;
-  image.alt = 'poster';
-  image.id = movieData.movieId;
+//   const image = document.createElement('img');
+//   image.src = url;
+//   image.alt = 'poster';
+//   image.id = movieData.movieId;
   
-  cardContent.appendChild(image);
-  cardContainer.appendChild(cardContent);
-  swiperSlide.appendChild(cardContainer);
-  swiperWrapper.appendChild(swiperSlide);
+//   cardContent.appendChild(image);
+//   cardContainer.appendChild(cardContent);
+//   swiperSlide.appendChild(cardContainer);
+//   swiperWrapper.appendChild(swiperSlide);
 
-  image.addEventListener("click", (e) => {
-    let targetE =e.target;
-    const movieId = e.target.id;
-    if (movieId) {
-      sendMovieIdToBackend(movieId); // Send movie ID to backend
-      window.location.href = `/movie/movieInfo/${(movieId)}`;
-    } else {
-      console.error("No movieId for this movie", movieData);
-    }
-  });
+//   image.addEventListener("click", (e) => {
+//     let targetE =e.target;
+//     const movieId = e.target.id;
+//     if (movieId) {
+//       sendMovieIdToBackend(movieId); // Send movie ID to backend
+//       window.location.href = `/movie/movieInfo/${(movieId)}`;
+//     } else {
+//       console.error("No movieId for this movie", movieData);
+//     }
+//   });
 
-}
+// }
 
-createSection();
+// createSection();
